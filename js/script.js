1fea8a4c25517ea06059139e16d67d922c90588e
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-03-15 18:22:16 +0100

    Add class actove to the clicked link

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict';
 
 const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -12,9 +12,12 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
 
