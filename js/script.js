aa16e8a8e025129c40b240c1125639fc22a9f3f3
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-03-19 14:13:28 +0100

    Add tag click handler algorithm

--- js/script.js
+++ js/script.js
@@ -126,7 +126,6 @@
 
       /* generate HTML of the link */
         const linkHTML = '<li><a href="#tag-' + tag + '"> ' + tag + ' </a></li> ';
-        console.log(linkHTML);
       /* add generated code to html variable */
         html = html + linkHTML;
     /* END LOOP: for each tag */
@@ -138,5 +137,45 @@
 }
 
   generateTags();
+
+  function tagClickHandler(event){
+  /* prevent default action for this event */
+
+  /* make new constant named "clickedElement" and give it the value of "this" */
+
+  /* make a new constant "href" and read the attribute "href" of the clicked element */
+
+  /* make a new constant "tag" and extract tag from the "href" constant */
+
+  /* find all tag links with class active */
+
+  /* START LOOP: for each active tag link */
+
+    /* remove class active */
+
+  /* END LOOP: for each active tag link */
+
+  /* find all tag links with "href" attribute equal to the "href" constant */
+
+  /* START LOOP: for each found tag link */
+
+    /* add class active */
+
+  /* END LOOP: for each found tag link */
+
+  /* execute function "generateTitleLinks" with article selector as argument */
+}
+
+function addClickListenersToTags(){
+  /* find all links to tags */
+
+  /* START LOOP: for each link */
+
+    /* add tagClickHandler as event listener for that link */
+
+  /* END LOOP: for each link */
+}
+
+addClickListenersToTags();
 
 }
